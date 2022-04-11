e614334c1ffc8204bb636dfc035f5073638dea12
lucciola200 <teona.odisharia@gmail.com>
2022-04-11 09:34:24 +0400

    test1

---
+++ src/com/company/hiperskill/MusicAdvisor/stage1/Main.java
@@ -0,0 +1,44 @@
+package com.company.hiperskill.MusicAdvisor.stage1;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String request = sc.nextLine();
+        while (!request.equals("exit")){
+            switch (request){
+                case "new":
+                    System.out.println("---NEW RELEASES---\n" +
+                            "Mountains [Sia, Diplo, Labrinth]\n" +
+                            "Runaway [Lil Peep]\n" +
+                            "The Greatest Show [Panic! At The Disco]\n" +
+                            "All Out Life [Slipknot]");
+                    break;
+                case "featured":
+                    System.out.println("---FEATURED---\n" +
+                            "Mellow Morning\n" +
+                            "Wake Up and Smell the Coffee\n" +
+                            "Monday Motivation\n" +
+                            "Songs to Sing in the Shower");
+                    break;
+                case "categories":
+                    System.out.println("---CATEGORIES---\n" +
+                            "Top Lists\n" +
+                            "Pop\n" +
+                            "Mood\n" +
+                            "Latin");
+                    break;
+                case "playlists Mood":
+                    System.out.println("---MOOD PLAYLISTS---\n" +
+                            "Walk Like A Badass  \n" +
+                            "Rage Beats  \n" +
+                            "Arab Mood Booster  \n" +
+                            "Sunday Stroll");
+                    break;
+            }
+            request =sc.nextLine();
+        }
+        System.out.println("---GOODBYE!---");
+    }
+}
